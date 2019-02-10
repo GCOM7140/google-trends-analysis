df0cb72cbb73f302f0f94fdb43dbfaa09ebd657c
Malcolm Mashig <mjm6jy@virginia.edu>
2019-02-09 22:32:11 -0500

    even more reproducible

--- R/match_trends.R
+++ R/match_trends.R
@@ -1,5 +1,11 @@
 # The objective is to compare google trends data and gtrendsR data
 # And to ensure they are identical
+# Note -- google trends takes a new sample every so often
+# So you must run gtrendsR when you download the google trends csv
+# if you want them to match
+# Here, you can either run your own test to see if they match by downloading
+# a new csv and running gtrendsR yourself
+# Or, you can use the csv files provided in the data folder that I used
 
 # Required packages
 
@@ -7,18 +13,23 @@ library(tidyverse)
 library(gtrendsR)
 
 # Specify google trend csv file path, search term(s), and timespan (2018 here)
-# You must download 2019-02-09-google-trends-homepage.csv found in 
-# the data folder and replace the path below to your own local path
+# Download 2019-02-09-google-trends-homepage.csv found in 
+# the data folder and replace the path below with your own local path
+# csv downloaded at https://bit.ly/2GzmiWc
+# Or provide your new csv path if you are running your own test
 
 csv <- "/Users/malcolm_mashig/Downloads/2019-02-09-google-trends-homepage.csv"
 search_term <- '"juice laundry"'
 time_span <- "2018-01-01 2018-12-31"
 
 # Read in google trends csv 
-# Specify column type so that it matches gtrends
+# If you want to run your own test with a new csv (not the one provided),
+# you must specify column type so that it matches gtrends
 
 google_trend <- read_csv(
-  csv, skip = 3, col_names = c('week_of', 'relative_interest')
+  csv, skip = 3, 
+  col_names = c('week_of', 'relative_interest')
+  # col_types = cols('relative_interest' = col_integer())
   ) %>% 
   as_tibble() %>% 
   mutate('search_term' = search_term) %>% 
@@ -30,23 +41,25 @@ gtrends_list <- gtrends(
   search_term,  geo = "US", time = time_span
 )
 
-# Write a csv to save gtrends data in data folder
+# Optional -- write a csv, if you are not using the one provided, to save 
+# gtrendsR data you use (remember, its a different sample each time)
 
 write_csv(gtrends_list[["interest_over_time"]], "2019-02-09-google-trends-gtrendsr.csv")
 
-# Convert gtrends interest_over_time into data table that matches google trend
+# If you are running your own test with new csv file, convert gtrends 
+# interest_over_time into data table that matches google trend
 
 gtrend <- gtrends_list[["interest_over_time"]] %>% 
-  as_tibble() %>% 
-  rename('relative_interest' = 'hits', 'week_of' = 'date', 
-         'search_term' = 'keyword') %>% 
-  select(c('week_of', 'search_term', 'relative_interest'))
+  as_tibble() %>%
+  select('relative_interest' = hits)
 
-# Instead of generating new gtrends sample, download csv I used
+# If you are not running your own test, download the csv I used
 # named '2019-02-09-google-trends-gtrendsr.csv' in the data folder
 # and insert your local path
 
 gtrendsr_csv <- '/Users/malcolm_mashig/Box Sync/google-trends-analysis/data/2019-02-09-google-trends-gtrendsr.csv'
+
+# and read it in
 
 gtrend <- read_csv(gtrendsr_csv) %>% 
   select('relative_interest' = hits)
