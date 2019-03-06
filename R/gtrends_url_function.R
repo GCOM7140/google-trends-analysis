9380c992079ab926f4f7f3d3e210692c9a3439e0
Malcolm Mashig <mjm6jy@virginia.edu>
2019-03-06 15:22:30 -0500

    url function

--- R/gtrends_url_function.R
+++ R/gtrends_url_function.R
@@ -1,1 +1,18 @@
-gtrends_url <- function(search_terms, start_date, end_date)
+library(tidyverse)
+
+gtrends_url <- function(start_date, end_date, geo, search_terms) {
+  str_c(
+    "https://trends.google.com/trends/explore?date=",
+    start_date,
+    "%20",
+    end_date,
+    "&geo=",
+    geo,
+    "&q=",
+    search_terms[1],
+    ",",
+    search_terms[2])
+  }
+
+gtrends_url("2016-10-09", "2018-12-31", "US", c("The%20Juice%20Laundry", 
+                                            "%22juice%20laundry%22"))
